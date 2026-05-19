087b5284741633e474ff28ead8f5bf6720e5070c
Jgrosskurth <159270531+Jgrosskurth@users.noreply.github.com>
2026-05-18 20:49:49 -0400

    feat: add migrated code for blocks/footer/footer.js

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,4 +1,3 @@
-import { getMetadata } from '../../scripts/aem.js';
 import { loadFragment } from '../fragment/fragment.js';
 
 /**
@@ -7,8 +6,8 @@ import { loadFragment } from '../fragment/fragment.js';
  */
 export default async function decorate(block) {
   // load footer as fragment
-  const footerMeta = getMetadata('footer');
-  const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
+  const footerMeta = document.querySelector('meta[name="footer"]');
+  const footerPath = footerMeta ? footerMeta.content : '/footer';
   const fragment = await loadFragment(footerPath);
 
   // decorate footer DOM
